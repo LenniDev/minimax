830567be0f80161aed71e9106b8e6f427e022c49
lennart.j.notingly <>
2021-11-28 15:46:01 +0100

    init

---
+++ minimax/__init__.py
@@ -0,0 +1,1 @@
+import minimax.minimax
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="minimax",
+    version="0.0.0",
+    description="python minimax implementation",
+    author="lennidev",
+    install_requires=[],
+    packages=find_packages()
+)
